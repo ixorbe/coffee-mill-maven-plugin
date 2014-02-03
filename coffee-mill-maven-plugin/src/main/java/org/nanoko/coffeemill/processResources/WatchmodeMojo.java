a14f9e31f96df7c8f70c6dcb2275726cb26de08f
Julien Etien <jetien@ubidreams.com>
2014-02-03 14:23:10 +0100

    add jetty server (for watch mode)

--- coffee-mill-maven-plugin/src/main/java/org/nanoko/coffeemill/processResources/WatchmodeMojo.java
+++ coffee-mill-maven-plugin/src/main/java/org/nanoko/coffeemill/processResources/WatchmodeMojo.java
@@ -1,34 +1,54 @@
 package org.nanoko.coffeemill.processResources;
 
-
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Execute;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.nanoko.coffeemill.mojos.AbstractCoffeeMillMojo;
 import org.nanoko.maven.WatchingException;
 import org.nanoko.maven.pipeline.Pipeline;
 import org.nanoko.maven.pipeline.Pipelines;
 
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 
 
 import java.io.IOException;
 
 /**
- * Run Mojo
+ * Watch mode Mojo
  */
-@Mojo(name = "run", threadSafe = false,
+@Mojo(name = "watch", threadSafe = false,
         requiresDependencyResolution = ResolutionScope.COMPILE,
         requiresProject = true
         )
 @Execute(phase = LifecyclePhase.PACKAGE)
-public class WatchmodeMojo extends  AbstractCoffeeMillMojo {
+public class WatchmodeMojo extends AbstractCoffeeMillMojo {
 
     private Pipeline pipeline;
+    
+    /**
+     * 
+     */
+    @Parameter(defaultValue="true")
+    protected boolean watchCoffeeScript;
+    
+    @Parameter(defaultValue="true")
+   protected boolean watchRunServer;
+    
+   @Parameter(defaultValue="${watch.port}")
+   protected int watchJettyServerPort=8234;
+   
+   //The Jetty Server
+   protected Server server;
 
-    public void execute() {
+    public void execute() throws MojoExecutionException {
        
     	/*try {
             init();
@@ -36,16 +56,53 @@ public class WatchmodeMojo extends  AbstractCoffeeMillMojo {
             throw new MojoExecutionException(e.getMessage(), e);
         }
 */
-        
+    	
+		if (watchRunServer) {
+	        try {
+	            server = new Server(watchJettyServerPort);
+	            addHandlersToServer();
+	            startServer();
+	        } catch (Exception e) {
+				throw new MojoExecutionException("Cannot run the jetty server", e);
+	        } 
+        } else {
+            try {
+                Thread.sleep(1000000000); // Pretty long
+            } catch (InterruptedException e) { /* ignore */ }
+        }
 
         pipeline.shutdown();
     }
+    
+    
 
     public void init() throws MojoExecutionException, WatchingException {
-        // Expand if needed.
-        
+        // Expand if needed.      
 
       //  pipeline = Pipelines.watchers(session, basedir, this).watch();
+    }
+    
+    
+    
+    private void addHandlersToServer() {
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setWelcomeFiles(new String[]{ "index.html" });
+        try {
+			resource_handler.setResourceBase(this.getWorkDirectory().getCanonicalPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
+        server.setHandler(handlers);
+    }
+    
+
+    private void startServer() throws Exception {
+        server.start();
+        server.join();
     }
 
 
