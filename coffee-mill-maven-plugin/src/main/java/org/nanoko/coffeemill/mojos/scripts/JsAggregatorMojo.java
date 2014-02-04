ecfd7751be294b600ac5c5f07a65538da958b01a
Rémi Parain <rparain@ubidreams.com>
2014-02-04 14:22:23 +0100

    add Js Aggregator
    Signed-off-by: Rémi Parain <rparain@ubidreams.com>

---
+++ coffee-mill-maven-plugin/src/main/java/org/nanoko/coffeemill/mojos/scripts/JsAggregatorMojo.java
@@ -0,0 +1,76 @@
+package org.nanoko.coffeemill.mojos.scripts;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+import org.nanoko.maven.WatchingException;
+import org.nanoko.coffeemill.mojos.AbstractCoffeeMillWatcherMojo;
+import org.nanoko.coffeemill.utils.FSUtils;
+import org.nanoko.coffeemill.utils.FileAggregation;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+
+/**
+ * Compiles Js files.
+ */
+@Mojo(name = "aggregate-javascript", threadSafe = false,
+        requiresDependencyResolution = ResolutionScope.TEST,
+        requiresProject = true,
+        defaultPhase = LifecyclePhase.PACKAGE)
+public class JsAggregatorMojo extends AbstractCoffeeMillWatcherMojo {
+
+    public void execute() throws MojoExecutionException {
+        try {
+            if ( this.stylesheetsDir.isDirectory()) {
+                this.aggregate();
+            }
+        } catch (WatchingException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+    }
+
+
+    public boolean accept(File file) {
+        return FSUtils.isInDirectory(file, this.workDir) && FSUtils.hasExtension(file, "js");
+    }
+
+    public void aggregate() throws WatchingException {
+    	getLog().info("Aggregate Js files from " + this.getWorkDirectory().getAbsolutePath());
+    	String fileName = this.project.getArtifactId()+"-"+this.project.getVersion();
+    	File output = new File( this.getWorkDirectory().getAbsolutePath()+File.separator+fileName+".js");
+        Collection<File> files = FileUtils.listFiles(this.getWorkDirectory(), new String[]{"js"}, true);
+        try {
+			FileAggregation.joinFiles( output, files);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+        if (!output.isFile()) {
+            throw new WatchingException("Error during the Js aggregation check log");
+        }
+    }
+
+
+    public boolean fileCreated(File file) throws WatchingException {
+    	this.aggregate();
+        return true;
+    }
+
+
+    public boolean fileUpdated(File file) throws WatchingException {
+    	this.aggregate();
+        return true;
+    }
+
+    public boolean fileDeleted(File file) throws WatchingException {
+    	this.aggregate();
+        return true;
+    }
+
+}
