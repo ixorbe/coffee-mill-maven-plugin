4939a73ad66011a3dc3ea368016324eb87e59479
Rémi Parain <rparain@ubidreams.com>
2014-02-04 14:21:50 +0100

    Add file aggregation tool
    Signed-off-by: Rémi Parain <rparain@ubidreams.com>

---
+++ coffee-mill-maven-plugin/src/main/java/org/nanoko/coffeemill/utils/FileAggregation.java
@@ -0,0 +1,51 @@
+package org.nanoko.coffeemill.utils;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collection;
+
+import org.apache.commons.io.IOUtils;
+
+/**
+ * 
+ * @author http://stackoverflow.com/a/14673198
+ *
+ */
+public class FileAggregation {
+	
+	  public static void joinFiles(File destination, Collection<File> sources)
+	            throws IOException {
+	        OutputStream output = null;
+	        try {
+	            output = createAppendableStream(destination);
+	            for (File source : sources) {
+	                appendFile(output, source);
+	            }
+	        } finally {
+	            IOUtils.closeQuietly(output);
+	        }
+	    }
+
+	    private static BufferedOutputStream createAppendableStream(File destination)
+	            throws FileNotFoundException {
+	        return new BufferedOutputStream(new FileOutputStream(destination, true));
+	    }
+
+	    private static void appendFile(OutputStream output, File source)
+	            throws IOException {
+	        InputStream input = null;
+	        try {
+	            input = new BufferedInputStream(new FileInputStream(source));
+	            IOUtils.copy(input, output);
+	        } finally {
+	            IOUtils.closeQuietly(input);
+	        }
+	    }
+}
