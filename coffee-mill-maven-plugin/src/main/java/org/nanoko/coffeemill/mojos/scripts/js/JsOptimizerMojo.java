3b80eb5a80e9d95f92e716737cc7fed3c1ced787
Rémi Parain <rparain@ubidreams.com>
2014-02-05 17:48:40 +0100

    add linking (replace old clojure compiler to NPM linter)
    Signed-off-by: Rémi Parain <rparain@ubidreams.com>

--- coffee-mill-maven-plugin/src/main/java/org/nanoko/coffeemill/mojos/scripts/js/JsOptimizerMojo.java
+++ coffee-mill-maven-plugin/src/main/java/org/nanoko/coffeemill/mojos/scripts/js/JsOptimizerMojo.java
@@ -16,30 +16,34 @@ import org.nanoko.coffeemill.mojos.AbstractCoffeeMillWatcherMojo;
 import org.nanoko.maven.WatchingException;
 
 import java.io.File;
+import java.util.Collection;
 
 
 import static org.nanoko.java.NPM.npm;
 
 /**
- * Minifying Js files.
+ * Optimize Js files.
  */
-@Mojo(name = "minify-javascript", threadSafe = false,
+@Mojo(name = "optimize-javascript", threadSafe = false,
         requiresDependencyResolution = ResolutionScope.COMPILE,
         requiresProject = true,
         defaultPhase = LifecyclePhase.PACKAGE)
-public class JsNPMMinifierMojo extends AbstractCoffeeMillWatcherMojo {
+public class JsOptimizerMojo extends AbstractCoffeeMillWatcherMojo {
 
-    public static final String PKG_NPM_NAME = "uglify-js";
-    public static final String PKG_NPM_VERSION = "2.4.12";
+    public static final String PKG_NPM_NAME = "jslint";
+    public static final String PKG_NPM_VERSION = "0.2.10";
     
-    private NPM ugly;
+    private NPM lint;
 
 
     public void execute() throws MojoExecutionException {
 
-    	ugly = npm(new MavenLoggerWrapper(this.getLog()), PKG_NPM_NAME, PKG_NPM_VERSION);
+    	lint = npm(new MavenLoggerWrapper(this.getLog()), PKG_NPM_NAME, PKG_NPM_VERSION);
         try {
-        	compile();
+        	Collection<File> files = FileUtils.listFiles(this.workDir, new String[]{"js"}, true);
+            for(File file : files)
+            	compile(file);
+
         } catch (WatchingException e) {
             throw new MojoExecutionException(e.getMessage(), e);
         }
@@ -49,40 +53,30 @@ public class JsNPMMinifierMojo extends AbstractCoffeeMillWatcherMojo {
         return  FSUtils.hasExtension(file, scriptExtensions);
     }
 
-    public void compile() throws WatchingException {
-    	
-    	String fileName = this.project.getArtifactId()+"-"+this.project.getVersion();
-    	File input = new File( this.getBuildDirectory().getAbsolutePath()+File.separator+fileName+".js");
+    public void compile(File f) throws WatchingException {
+    	String name = f.getName().substring(0, f.getName().lastIndexOf('.'))+".js";
+    	File input = new File( this.getBuildDirectory().getAbsolutePath(),name);
     	if(!input.exists())
     		return;
-    	File output = new File( this.getBuildDirectory().getAbsolutePath()+File.separator+fileName+"-min.js");
- 	
-    	if(output.exists())
-    		FileUtils.deleteQuietly(output);
-    	
-        getLog().info("Minifying " + input.getAbsolutePath() + " to " + output.getAbsolutePath());
-        int exit = ugly.execute("uglifyjs",input.getAbsolutePath(), "-o",  output.getAbsolutePath(),"-c");
-		getLog().debug("Js minification execution exiting with " + exit + " status");
 
-        if (!output.isFile()) {
-            throw new WatchingException("Error during the minification of " + input.getAbsoluteFile() + " check log");
-        }
+        getLog().info("Linting " + input.getAbsolutePath());
+        int exit = lint.execute("jslint",input.getAbsolutePath());
+
     }
 
 
     public boolean fileCreated(File file) throws WatchingException {
-        compile();
+        compile(file);
         return true;
     }
 
 
     public boolean fileUpdated(File file) throws WatchingException {
-        compile();
+        compile(file);
         return true;
     }
 
     public boolean fileDeleted(File file) throws WatchingException{
-    	compile();
         return true;
     }
 
