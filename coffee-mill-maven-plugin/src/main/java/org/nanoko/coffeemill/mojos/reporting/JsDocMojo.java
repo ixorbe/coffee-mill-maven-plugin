ca5a55103abc7694cd580737242b620cd0e6ceb4
Rémi Parain <rparain@ubidreams.com>
2014-02-06 17:32:15 +0100

    add JsDoc generation Mojo
    Signed-off-by: Rémi Parain <rparain@ubidreams.com>

---
+++ coffee-mill-maven-plugin/src/main/java/org/nanoko/coffeemill/mojos/reporting/JsDocMojo.java
@@ -0,0 +1,89 @@
+package org.nanoko.coffeemill.mojos.reporting;
+
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.nanoko.java.NPM;
+
+import org.nanoko.coffeemill.utils.FSUtils;
+import org.nanoko.coffeemill.utils.MavenLoggerWrapper;
+import org.nanoko.coffeemill.mojos.AbstractCoffeeMillWatcherMojo;
+
+import org.nanoko.maven.WatchingException;
+
+import java.io.File;
+
+
+import static org.nanoko.java.NPM.npm;
+
+/**
+ * Minifying Js files.
+ */
+@Mojo(name = "documentation-javascript", threadSafe = false,
+        requiresDependencyResolution = ResolutionScope.TEST,
+        requiresProject = true,
+        defaultPhase = LifecyclePhase.PACKAGE)
+public class JsDocMojo extends AbstractCoffeeMillWatcherMojo {
+
+    public static final String PKG_NPM_NAME = "jsdoc";
+    public static final String PKG_NPM_VERSION = "3.3.0-alpha4";
+    
+    private NPM jsdoc;
+    
+    public String inputFilename = null;
+    public String outputDir = null;
+
+    public void execute() throws MojoExecutionException {
+
+    	jsdoc = npm(new MavenLoggerWrapper(this.getLog()), PKG_NPM_NAME, PKG_NPM_VERSION);
+        try {
+        	compile();
+        } catch (WatchingException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+    }
+
+    public boolean accept(File file) {
+        return  FSUtils.hasExtension(file, scriptExtensions);
+    }
+
+    public void compile() throws WatchingException {
+    	if(inputFilename == null)
+    		inputFilename = this.project.getArtifactId()+"-"+this.project.getVersion();
+    	
+    	File input = new File( this.getBuildDirectory(), inputFilename+".js");
+    	if(!input.exists())
+    		return;
+    	File output = new File( this.buildDirectory,"jsdoc-report" );
+ 	
+    	if(output.exists())
+    		FileUtils.deleteQuietly(output);
+    	
+        getLog().info("Make Js Doc for " + input.getAbsolutePath() );
+        int exit = jsdoc.execute("jsdoc", input.getAbsolutePath(), "-d",  output.getAbsolutePath() );
+		getLog().debug("Js Doc generation execution exiting with " + exit + " status");
+
+    }
+
+
+    public boolean fileCreated(File file) throws WatchingException {
+        compile();
+        return true;
+    }
+
+
+    public boolean fileUpdated(File file) throws WatchingException {
+        compile();
+        return true;
+    }
+
+    public boolean fileDeleted(File file) throws WatchingException{
+    	compile();
+        return true;
+    }
+
+}
