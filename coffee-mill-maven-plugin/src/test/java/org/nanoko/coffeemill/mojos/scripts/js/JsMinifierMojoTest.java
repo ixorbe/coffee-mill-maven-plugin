4518d3b22629670ba312a0df5a3593bb689b9168
Julien Etien <jetien@ubidreams.com>
2014-02-07 12:53:06 +0100

    add JsMinifier unit test

---
+++ coffee-mill-maven-plugin/src/test/java/org/nanoko/coffeemill/mojos/scripts/js/JsMinifierMojoTest.java
@@ -0,0 +1,52 @@
+package org.nanoko.coffeemill.mojos.scripts.js;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JsMinifierMojoTest {
+
+	private final File jsFileToMinify = new File("src/test/resources/js/h-ubu.js");
+	private final String testDir = "target/test/JsMinifierMojoTest/";
+	private final File workDir = new File(testDir + "/tmp");
+	private final File buildDir = new File(testDir + "/www");
+	private JsMinifierMojo mojo;
+	
+	@Before
+	public void prepareTestDirectory(){  	
+    	this.mojo = new JsMinifierMojo();        
+    	this.mojo.setWorkDirectory(this.workDir);
+    	this.mojo.setBuildDirectory(this.buildDir);
+    	
+    	
+    	if(this.jsFileToMinify.exists())
+			try {
+				FileUtils.copyFileToDirectory(this.jsFileToMinify, this.workDir);
+			} catch (IOException e) { e.printStackTrace(); } 
+    }
+	
+	@Test
+    public void testJavaScriptMinification() throws MojoExecutionException, MojoFailureException {  
+    	System.out.println("\n ==> Should minify file \"h-ubu.js\" from "+this.workDir+" to "+this.buildDir);  
+    	this.mojo.inputFileName = "h-ubu";
+    	this.mojo.execute();    	
+
+    	assertTrue(new File(this.mojo.getBuildDirectory(), "h-ubu-min.js").exists());
+    }
+	
+	/*@After
+	public void cleanTestDirectory() {
+		if(this.mojo.getWorkDirectory().exists())
+			FileUtils.deleteQuietly(this.mojo.getWorkDirectory());
+	}*/
+	
+}
